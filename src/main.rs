228b2b381744ff293bc5ba6e39ab65b88bf41dd6
DewaldV <dewald.viljoen@pm.me>
2024-03-23 12:29:26 +0000

    Use our fork of the monzo-lib

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,14 @@ use std::env;
 async fn main() -> monzo::Result<()> {
     let token = env::var("MONZO_ACCESS_TOKEN").expect("$MONZO_ACCESS_TOKEN is not set");
 
-    // You can create a simple monzo client using only an access token
-    let quick_client = Client::new(token);
+    let client = Client::new(token).with_url("http://foo.bar.nope.not-a-thing");
 
-    // get a list of accounts
-    let accounts = quick_client.accounts().await?;
+    let accounts = client.accounts().await?;
     dbg!(&accounts);
 
-    // get the id of one of the accounts
     let account_id = &accounts[1].id;
 
-    // get the balance of that account
-    let balance = quick_client.balance(account_id).await?;
+    let balance = client.balance(account_id).await?;
     dbg!(&balance);
 
     Ok(())
