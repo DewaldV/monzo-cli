aab5d237c2bdfde020986229d1973532244efdf5
DewaldV <dewald.viljoen@pm.me>
2024-04-27 15:11:56 +0100

    feat: print out batch row before executing

--- src/batch.rs
+++ src/batch.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{fmt::Display, path::Path};
 
 use crate::{accounts, currency::Amount, error::Result};
 
@@ -10,14 +10,33 @@ struct Row {
     amount: Amount,
 }
 
-pub async fn run(_token: String, file: String) -> Result<()> {
+impl Display for Row {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "account={}, category={}, description={}, amount={}",
+            self.account.value(),
+            self.category,
+            self.description,
+            self.amount
+        )
+    }
+}
+
+pub async fn run(token: String, file: String) -> Result<()> {
     let path = Path::new(&file);
-    println!("batch file path: {}", path.display());
+    println!("starting batch run for file: {}", path.display());
 
     let mut reader = csv::Reader::from_path(path)?;
     for result in reader.deserialize() {
         let row: Row = result?;
-        dbg!(row);
+
+        println!("executing batch row, {}", row);
+
+        // let pot_name = config::pot_for(row.account, row.category);
+        // let deposit = pots::deposit(&token, pot_name, &row.amount).await?;
+        // let deposit_tx = transactions::find_transaction(&token, depo).await?;
+        // transactions::annotate(&token, &deposit_tx.id, row.description).await?;
     }
     Ok(())
 }
